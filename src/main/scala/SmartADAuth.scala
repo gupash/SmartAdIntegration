b2ddbc7b1f13a6452cde1d263d892bec238ca382
Ashish Gupta <agupta@imvu.com>
2021-06-30 21:32:01 -0700

    Finally Done

--- src/main/scala/SmartADAuth.scala
+++ src/main/scala/SmartADAuth.scala
@@ -10,6 +10,7 @@ import akka.pattern.ask
 import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import akka.util.Timeout
+import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.{Logger, LoggerFactory}
 import spray.json.DefaultJsonProtocol._
 import spray.json.{RootJsonFormat, enrichAny}
@@ -30,37 +31,42 @@ object SmartADAuth {
   val s3SegmentDataBucket: String = "imvudata"
   val s3SegmentDataPrefix: String = "user/hive/warehouse/marketing.db/smartad_segment_data/"
 
+  val customConf: Config = ConfigFactory.parseString("""
+  akka {
+    http.host-connection-pool {
+      max-connections = 4
+    }
+    http.client.idle-timeout = 60s
+  }""".stripMargin)
+
   val localS3FilesDir = new java.io.File(localS3FilePath)
 
   implicit val profile: RootJsonFormat[Profile] = jsonFormat2(Profile)
   implicit val profiles: RootJsonFormat[Profiles] = jsonFormat1(Profiles)
-  implicit val system: ActorSystem = ActorSystem("SmartAd")
+  implicit val system: ActorSystem = ActorSystem("SmartAd", ConfigFactory.load(customConf))
   implicit val mat: Materializer = Materializer(system)
   implicit val executionContext: ExecutionContextExecutor = mat.executionContext
 
+  // Setting https protocol
   val sslCtx: HttpsConnectionContext = ConnectionContext.httpsClient(SSLContext.getDefault)
   Http().setDefaultClientHttpsContext(sslCtx)
 
   def main(args: Array[String]): Unit = {
 
     if (args.length < 2) sys.error("Please provide client_id and client_secret as program input")
-
     val smartADActor: ActorRef = system.actorOf(Props[SmartAdAuthActor], "smart-ad-auth-actor")
-
     log.info("Started Application")
 
     val clientId = args(0)
     val clientSecret = args(1)
 
-    //Downloading segment files from s3
+    // Downloading segment files from s3
     //downloadSegmentData()
 
     log.info("Old files cleared from s3 files download dir")
 
     val txtFiles: Iterator[File] = localS3FilesDir.listFiles.filter(_.getName.endsWith(".txt")).iterator
-
     val data: Iterator[String] = txtFiles.map(scala.io.Source.fromFile).flatMap(_.getLines())
-
     val dataGroup: data.GroupedIterator[String] = data.grouped(1000)
 
     val groupedProfile: Iterator[Profiles] = for {
@@ -72,8 +78,10 @@ object SmartADAuth {
 
     val segmentJson: Iterator[String] = groupedProfile.map(_.toJson.toString)
 
+    // Timeout for the smart AD auth actor to return response within
     implicit val timeout: Timeout = Timeout(2.second)
 
+    // Scheduler which runs the update bearer token actor every 10 minutes
     system.scheduler.scheduleWithFixedDelay(
       delay = 10.minutes,
       initialDelay = 0.nanoseconds,
@@ -82,9 +90,12 @@ object SmartADAuth {
     )
 
     def bearer: Future[String] = (smartADActor ? GetToken).mapTo[Token].map(_.token)
-
     log.info("Starting Upload (it may take around 15-20 minutes) ...")
 
+    /* Main part where we are creating akka source from file iterator and uploading 1 row (1000 profiles)
+     * in 1 future as there is a limit on smartAd side to upload max 1000 profiles per second.
+     * Finally we are counting the total operations
+     */
     val processStatus: Future[Int] = Source
       .fromIterator(() => segmentJson)
       .throttle(1, 1.seconds)
@@ -92,12 +103,7 @@ object SmartADAuth {
       .map(_ => 1)
       .runFold(0)(_ + _)
 
-    def shutDownHook: Future[Terminated] =
-      Http().shutdownAllConnectionPools().flatMap { _ =>
-        mat.shutdown()
-        system.terminate()
-      }
-
+    // Captures time taken by the upload segment future for all profiles
     timedFuture(processStatus)
 
     processStatus.onComplete { res =>
@@ -151,4 +157,10 @@ object SmartADAuth {
     future.onComplete(_ => log.info(s"Future took ${System.currentTimeMillis() - start} ms"))
     future
   }
+
+  def shutDownHook: Future[Terminated] =
+    Http().shutdownAllConnectionPools().flatMap { _ =>
+      mat.shutdown()
+      system.terminate()
+    }
 }
